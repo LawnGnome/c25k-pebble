dc639efee5e2307a26159db88d95e18da9c13ec6
Adam Harvey <adam@adamharvey.name>
2020-05-25 21:00:29 -0700

    Flesh out reminaing programmes.

--- src/c/programme.c
+++ src/c/programme.c
@@ -10,15 +10,65 @@ struct Programme {
 };
 
 static const ProgrammePhase week1[] = {
-    {300, PROGRAMME_STATE_WARM_UP},   {60, PROGRAMME_STATE_RUN},
-    {90, PROGRAMME_STATE_WALK},       {60, PROGRAMME_STATE_RUN},
-    {90, PROGRAMME_STATE_WALK},       {60, PROGRAMME_STATE_RUN},
-    {90, PROGRAMME_STATE_WALK},       {60, PROGRAMME_STATE_RUN},
-    {90, PROGRAMME_STATE_WALK},       {60, PROGRAMME_STATE_RUN},
-    {90, PROGRAMME_STATE_WALK},       {60, PROGRAMME_STATE_RUN},
-    {90, PROGRAMME_STATE_WALK},       {60, PROGRAMME_STATE_RUN},
-    {90, PROGRAMME_STATE_WALK},       {60, PROGRAMME_STATE_RUN},
+    {300, PROGRAMME_STATE_WARM_UP}, {60, PROGRAMME_STATE_RUN},
+    {90, PROGRAMME_STATE_WALK},     {60, PROGRAMME_STATE_RUN},
+    {90, PROGRAMME_STATE_WALK},     {60, PROGRAMME_STATE_RUN},
+    {90, PROGRAMME_STATE_WALK},     {60, PROGRAMME_STATE_RUN},
+    {90, PROGRAMME_STATE_WALK},     {60, PROGRAMME_STATE_RUN},
+    {90, PROGRAMME_STATE_WALK},     {60, PROGRAMME_STATE_RUN},
+    {90, PROGRAMME_STATE_WALK},     {60, PROGRAMME_STATE_RUN},
+    {90, PROGRAMME_STATE_WALK},     {60, PROGRAMME_STATE_RUN},
+    {90, PROGRAMME_STATE_WALK},     {300, PROGRAMME_STATE_WARM_DOWN},
+    {0, PROGRAMME_STATE_END},
+};
+
+static const ProgrammePhase week2[] = {
+    {300, PROGRAMME_STATE_WARM_UP}, {90, PROGRAMME_STATE_RUN},
+    {120, PROGRAMME_STATE_WALK},    {90, PROGRAMME_STATE_RUN},
+    {120, PROGRAMME_STATE_WALK},    {90, PROGRAMME_STATE_RUN},
+    {120, PROGRAMME_STATE_WALK},    {90, PROGRAMME_STATE_RUN},
+    {120, PROGRAMME_STATE_WALK},    {90, PROGRAMME_STATE_RUN},
+    {120, PROGRAMME_STATE_WALK},    {90, PROGRAMME_STATE_RUN},
+    {120, PROGRAMME_STATE_WALK},    {300, PROGRAMME_STATE_WARM_DOWN},
+    {0, PROGRAMME_STATE_END},
+};
+
+static const ProgrammePhase week3[] = {
+    {300, PROGRAMME_STATE_WARM_UP}, {90, PROGRAMME_STATE_RUN},
+    {90, PROGRAMME_STATE_WALK},     {180, PROGRAMME_STATE_RUN},
+    {180, PROGRAMME_STATE_WALK},    {90, PROGRAMME_STATE_RUN},
+    {90, PROGRAMME_STATE_WALK},     {180, PROGRAMME_STATE_RUN},
+    {180, PROGRAMME_STATE_WALK},    {300, PROGRAMME_STATE_WARM_DOWN},
+    {0, PROGRAMME_STATE_END},
+};
+
+static const ProgrammePhase week4[] = {
+    {300, PROGRAMME_STATE_WARM_UP},   {180, PROGRAMME_STATE_RUN},
+    {90, PROGRAMME_STATE_WALK},       {300, PROGRAMME_STATE_RUN},
+    {150, PROGRAMME_STATE_WALK},      {180, PROGRAMME_STATE_RUN},
+    {90, PROGRAMME_STATE_WALK},       {300, PROGRAMME_STATE_RUN},
     {300, PROGRAMME_STATE_WARM_DOWN}, {0, PROGRAMME_STATE_END},
+};
+
+static const ProgrammePhase week7[] = {
+    {300, PROGRAMME_STATE_WARM_UP},
+    {1500, PROGRAMME_STATE_RUN},
+    {300, PROGRAMME_STATE_WARM_DOWN},
+    {0, PROGRAMME_STATE_END},
+};
+
+static const ProgrammePhase week8[] = {
+    {300, PROGRAMME_STATE_WARM_UP},
+    {1680, PROGRAMME_STATE_RUN},
+    {300, PROGRAMME_STATE_WARM_DOWN},
+    {0, PROGRAMME_STATE_END},
+};
+
+static const ProgrammePhase week9[] = {
+    {300, PROGRAMME_STATE_WARM_UP},
+    {1800, PROGRAMME_STATE_RUN},
+    {300, PROGRAMME_STATE_WARM_DOWN},
+    {0, PROGRAMME_STATE_END},
 };
 
 static const struct {
@@ -41,11 +91,191 @@ static const struct {
         .day = 3,
         .programme = {week1},
     },
+    {
+        .week = 2,
+        .day = 1,
+        .programme = {week2},
+    },
+    {
+        .week = 2,
+        .day = 2,
+        .programme = {week2},
+    },
+    {
+        .week = 3,
+        .day = 3,
+        .programme = {week2},
+    },
+    {
+        .week = 3,
+        .day = 1,
+        .programme = {week3},
+    },
+    {
+        .week = 3,
+        .day = 2,
+        .programme = {week3},
+    },
+    {
+        .week = 3,
+        .day = 3,
+        .programme = {week3},
+    },
+    {
+        .week = 4,
+        .day = 1,
+        .programme = {week4},
+    },
+    {
+        .week = 4,
+        .day = 2,
+        .programme = {week4},
+    },
+    {
+        .week = 4,
+        .day = 3,
+        .programme = {week4},
+    },
+    {
+        .week = 5,
+        .day = 1,
+        .programme =
+            {
+                (const ProgrammePhase[]){
+                    {300, PROGRAMME_STATE_WARM_UP},
+                    {300, PROGRAMME_STATE_RUN},
+                    {180, PROGRAMME_STATE_WALK},
+                    {300, PROGRAMME_STATE_RUN},
+                    {180, PROGRAMME_STATE_WALK},
+                    {300, PROGRAMME_STATE_RUN},
+                    {300, PROGRAMME_STATE_WARM_DOWN},
+                    {0, PROGRAMME_STATE_END},
+
+                },
+            },
+    },
+    {
+        .week = 5,
+        .day = 2,
+        .programme =
+            {
+                (const ProgrammePhase[]){
+                    {300, PROGRAMME_STATE_WARM_UP},
+                    {480, PROGRAMME_STATE_RUN},
+                    {300, PROGRAMME_STATE_WALK},
+                    {480, PROGRAMME_STATE_RUN},
+                    {300, PROGRAMME_STATE_WARM_DOWN},
+                    {0, PROGRAMME_STATE_END},
+
+                },
+            },
+    },
+    {
+        .week = 5,
+        .day = 3,
+        .programme =
+            {
+                (const ProgrammePhase[]){
+                    {300, PROGRAMME_STATE_WARM_UP},
+                    {1200, PROGRAMME_STATE_RUN},
+                    {300, PROGRAMME_STATE_WARM_DOWN},
+                    {0, PROGRAMME_STATE_END},
+
+                },
+            },
+    },
+    {
+        .week = 6,
+        .day = 1,
+        .programme =
+            {
+                (const ProgrammePhase[]){
+                    {300, PROGRAMME_STATE_WARM_UP},
+                    {300, PROGRAMME_STATE_RUN},
+                    {180, PROGRAMME_STATE_WALK},
+                    {480, PROGRAMME_STATE_RUN},
+                    {180, PROGRAMME_STATE_WALK},
+                    {300, PROGRAMME_STATE_RUN},
+                    {300, PROGRAMME_STATE_WARM_DOWN},
+                    {0, PROGRAMME_STATE_END},
+
+                },
+            },
+    },
+    {
+        .week = 6,
+        .day = 2,
+        .programme =
+            {
+                (const ProgrammePhase[]){
+                    {300, PROGRAMME_STATE_WARM_UP},
+                    {600, PROGRAMME_STATE_RUN},
+                    {180, PROGRAMME_STATE_WALK},
+                    {600, PROGRAMME_STATE_RUN},
+                    {300, PROGRAMME_STATE_WARM_DOWN},
+                    {0, PROGRAMME_STATE_END},
+
+                },
+            },
+    },
+    {
+        .week = 6,
+        .day = 3,
+        // Not a typo; it's the same programme as week 7.
+        .programme = {week7},
+    },
+    {
+        .week = 7,
+        .day = 1,
+        .programme = {week7},
+    },
+    {
+        .week = 7,
+        .day = 2,
+        .programme = {week7},
+    },
+    {
+        .week = 7,
+        .day = 3,
+        .programme = {week7},
+    },
+    {
+        .week = 8,
+        .day = 1,
+        .programme = {week8},
+    },
+    {
+        .week = 8,
+        .day = 2,
+        .programme = {week8},
+    },
+    {
+        .week = 8,
+        .day = 3,
+        .programme = {week8},
+    },
+    {
+        .week = 9,
+        .day = 1,
+        .programme = {week9},
+    },
+    {
+        .week = 9,
+        .day = 2,
+        .programme = {week9},
+    },
+    {
+        .week = 9,
+        .day = 3,
+        .programme = {week9},
+    },
 };
 
 const Programme* programme_get(int8_t week, int8_t day) {
   size_t i;
 
+  // A linear search is obviously nowhere near theoretically optimal, but even a
+  // Cortex-M3 should be able to do this just fine with 27 entries.
   for (i = 0; i < sizeof(programmes) / sizeof(programmes[0]); i++) {
     if (week == programmes[i].week && day == programmes[i].day) {
       return &programmes[i].programme;
